ceea3d8e06ede112fc4e7f3cf9b8232f162ddf42
Nico-dsa <6463@holbertonstudents.com>
2023-03-27 02:06:55 -0700

    Modification main.h

--- main.h
+++ main.h
@@ -1,7 +1,21 @@
 #ifndef PRINTF
 #define PRINTF
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * strut types - Structure for printf
+ * @p: pointers to characteres
+ * @func: function pointer to print fucntions
+ */
+
+typedef struct types
+{
+	char p;
+	int (*func)(va_list);
+} print_f;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
