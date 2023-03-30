01bc687ff8aea4368386efdd9378b654e820ff19
nairbrh <houcinenair@gmail.com>
2023-03-30 03:13:23 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,49 +1,49 @@
 #include "main.h"
 /**
- * _printf - produces output according to a format
- * @format: character string containing the directives
- *
- * Return: the number of characters printed
+ * _printf - prints output according to a format.
+ * @format: character string.
+ * Return: number of characters printed.
  */
+
 int _printf(const char *format, ...)
 {	va_list args;
 	int (*printer)(va_list);
 	int count = 0;
 
 	if (format == NULL)
-	return (-1);
+		return (-1);
 	va_start(args, format);
 	while (*format)
 	{
-	if (*format == '%')
-	{
-	format++;
-	printer = get_func(format);
+		if (*format == '%')
+		{
+			format++;
+			printer = get_func(format);
 
-	if (printer == NULL)
-	{
-		if (*format == '\0')
-			return (-1);
-		if (*format == '!')
+			if (printer == NULL)
 			{
-				count += _putchar('!');
+				if (*format == '\0')
+					return (-1);
+				if (*format == '!')
+				{
+					count += _putchar('!');
+					format++;
+					continue;
+				}
+				count += _putchar('%');
+			}
+			else
+			{
+				count += printer(args);
 				format++;
 				continue;
 			}
-			count += _putchar('%');
-	}
+		}
 		else
 		{
-	count += printer(args);
-	format++;
-	continue;
+			count += _putchar(*format);
 		}
-	}
-	else
-	{
-		count += _putchar(*format);
-	}
-	format++;
+		format++;
 	}
 	va_end(args);
 	return (count);
